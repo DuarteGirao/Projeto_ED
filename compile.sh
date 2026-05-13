7a62f703c464e07aa265565b05e736a69d6ae1b2
DuarteGirao <pv31525@alunos.estgv.ipv.pt>
2026-05-13 13:08:27 +0100

    Remove unused source files and compilation script
    - Deleted `caixa.c`, `cliente.c`, `pessoa.c`, `produto.c`, `relogio.c`, `supermercado.c`, and `uteis.c` as they are no longer needed.
    - Removed the `compile.sh` script since the build process has changed.

--- compile.sh
+++
@@ -1,3 +0,0 @@
-gcc main.c codigo/produto.c codigo/cliente.c codigo/uteis.c codigo/pessoa.c codigo/supermercado.c codigo/relogio.c codigo/caixa.c -Iheaders -o main.exe
-echo ""
-echo "Fim do ficheiro de compilação!"
